c6edc2830a790947a5e70f15343367aea6740063
xrenmorzhov <kom5583@gmail.com>
2023-03-16 22:18:33 +0300

    'add new commit'

--- str_func.py
+++ str_func.py
@@ -1,6 +1,22 @@
 def capital(word):
+    """
+    :param word:
+    :return:
+    """
     return word.upper()
 
 
 word = "fjfjfjf"
 print(capital(word))
+
+
+def capital_2(word):
+    """
+    :param word:
+    :return:
+    """
+    return word.title()
+
+
+word = "fjfjfjf dkfkfgjfk fgker"
+print(capital_2(word))
